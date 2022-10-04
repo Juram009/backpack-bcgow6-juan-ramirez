22ecd052a477d51561859abb66ca3726212bdce8
Juram009 <juanjose.ramirez@mercadolibre.com.co>
2022-10-04 16:27:21 -0500

    GO WEB I TARDE (03/10/22)

--- Go Web/Practicas/main.go
+++ Go Web/Practicas/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Producto struct {
-	Id              string
-	Nombre          string
-	Color           string
-	Precio          int
-	Stock           int
-	Codigo          string
-	Publicado       bool
-	FechaDeCreacion string
+	Id              string `json:"id"`
+	Nombre          string `json:"nombre"`
+	Color           string `json:"color"`
+	Precio          int    `json:"precio"`
+	Stock           int    `json:"stock"`
+	Codigo          string `json:"codigo"`
+	Publicado       bool   `json:"publicado"`
+	FechaDeCreacion string `json:"fechaDeCreacion"`
 }
 
 func main() {
@@ -25,21 +27,62 @@ func main() {
 		})
 	})
 	router.GET("/productos", GetAll)
+	router.GET("/productos/:Id", GetOne)
 	router.Run()
 
 	router = gin.Default()
 }
-func generateProducts() []Producto {
-	products := []Producto{
-		{"1", "Televisor", "Negro", 1000000, 57, "df34jh174", true, "07/04/2020"},
-		{"2", "Repisa", "Negro", 40000, 12, "jk3h4gf576", true, "13/08/2021"},
+func getProducts() ([]Producto, error) {
+	var productos []Producto
+	data, err := os.ReadFile("./products.json")
+	if err != nil {
+		return nil, err
 	}
-	return products
+
+	err = json.Unmarshal(data, &productos)
+	if err != nil {
+		return nil, err
+	}
+
+	return productos, nil
 }
 
 func GetAll(ctx *gin.Context) {
-	products := generateProducts()
+	products, err := getProducts()
+	var filtrados []*Producto
+	for _, e := range products {
+		if ctx.GetBool("Publicado") != e.Publicado {
+			filtrados = append(filtrados, &e)
+		}
+	}
+	if err != nil {
+		ctx.String(400, "Error in JSON")
+	}
 	ctx.JSON(http.StatusOK, gin.H{
-		"Productos": products,
+		"Productos": filtrados,
 	})
+}
+
+func GetOne(ctx *gin.Context) {
+	productos, err := getProducts()
+	if err != nil {
+		ctx.String(404, "Producto no encontrado")
+		return
+	}
+	id := ctx.Param("Id")
+	if err != nil {
+		ctx.String(404, "Producto no encontrado")
+		return
+	}
+	for _, value := range productos {
+		if id == value.Id {
+			ctx.JSON(200, value)
+			return
+		}
+	}
+	ctx.String(404, "Producto no encontrado")
+
+}
+func filter() {
+
 }
